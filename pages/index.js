84bcc22afed4ae4faa44d667205026f7536c81bb
Angeles Maza <angie.maza.89@gmail.com>
2023-03-06 21:49:13 -0300

    third-commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import useSWR from 'swr';
 const Home = () => {
 
   const fetcher = (url) => fetch(url).then((res) => res.json());
-  const url = 'http://localhost:3000/api/recipes';
+  const url = 'http://https://clase7-44jb.vercel.app/api/recipes';
 
   const { data, error, isLoading } = useSWR(url, fetcher);
   if (error) return <div>failed to load</div>
